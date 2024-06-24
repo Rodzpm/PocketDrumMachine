78bbba8c07f76680ef85deefcf3ba7a013f90079
Rodzpm <allan.leherpeuxprv@gmail.com>
2024-06-24 12:01:28 +0200

    feat: added first sequencer system with save state
    Co-authored-by: Marlon830 <Marlon830@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "Tools/BPM/BPM.hpp"
 #include "Inputs/Button/Button.hpp"
 #include "Outputs/Led/Led.hpp"
+#include <Chrono.h>
 
 //Objectif:
 //appui sur bouton edit tu cliques sur les boutons ça les activent ou désactivent
@@ -10,44 +11,61 @@
 
 //TODO: Placer les résistances plus éloignées elles font un court circuit
 
-Tools::BPM bpm(100, 60, 2000);
+Tools::BPM bpm(150, 60, 2000);
 int index = 0;
 
-Inputs::Button editButton(12);
+Inputs::Button editButton(2);
 bool editMode = false;
+Chrono myChrono;
 LinkedList<int> ledsState;
 LinkedList<Outputs::Led *> leds;
 LinkedList<Inputs::Button *> sequencer;
 
 void setup() {
 	Serial.begin(9600);
-  ledsState.add(0);
   ledsState.add(1);
-  ledsState.add(0);
-  ledsState.add(0);
-  leds.add(new Outputs::Led(8));
+  ledsState.add(1);
+  ledsState.add(1);
+  ledsState.add(1);
+  leds.add(new Outputs::Led(5));
+  leds.add(new Outputs::Led(6));
   leds.add(new Outputs::Led(9));
   leds.add(new Outputs::Led(10));
-  leds.add(new Outputs::Led(11));
 
-  sequencer.add(new Inputs::Button(2));
   sequencer.add(new Inputs::Button(3));
   sequencer.add(new Inputs::Button(4));
-  sequencer.add(new Inputs::Button(5));
+  sequencer.add(new Inputs::Button(7));
+  sequencer.add(new Inputs::Button(8));
 }
 
 
 void loop() {
-  if (editButton.get() == Inputs::RELEASED) {
+  int value = editButton.get();
+  if (value == Inputs::RELEASED) {
     editMode = !editMode;
+    for (int i = 0; i != 4; i++) {
+      leds.get(i)->set(LOW);
+    }
   }
   if (!editMode) {
-    Serial.println("CACA");
-    leds.get(0)->set(ledsState.get(0));
-    // for (int i = 0; i != 4; i++) {
-    //   leds.get(i)->set(ledsState.get(i));
-    //   delay(bpm.getBPMToMil());
-    //   leds.get(i)->set(LOW);
-    // }
+    leds.get(index)->set(ledsState.get(index));
+    if (myChrono.hasPassed(bpm.getBPMToMil())) {
+      leds.get(index)->set(LOW);
+      myChrono.restart();
+      index += 1;
+      if (index == 4) {
+        index = 0;
+      }
+    }
+  } else {
+    for (int i = 0; i != 4; i++) {
+      int value = sequencer.get(i)->get();
+      if (value == Inputs::RELEASED) {
+        ledsState.set(i, !ledsState.get(i));
+      }
+    }
+    for (int i = 0; i != 4; i++) {
+      leds.get(i)->set(ledsState.get(i));
+    }
   }
 }
