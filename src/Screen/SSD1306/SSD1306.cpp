8ebf52ac124e897f7f456a7cbfe2bb78de7956e3
Rodzpm <allan.leherpeuxprv@gmail.com>
2024-08-16 15:30:22 +0200

    feat: oled screen with pico

--- src/Screen/SSD1306/SSD1306.cpp
+++ src/Screen/SSD1306/SSD1306.cpp
@@ -1,52 +1,69 @@
-// #include "SSD1306.hpp"
+#include "SSD1306.hpp"
 
-// namespace Screen {
-//     SSD1306::SSD1306(int width, int height, int resetPin) {
-//         this->screenWidth = width;
-//         this->screenHeight = height;
-//         this->display = Adafruit_SSD1306(width, height, &Wire, resetPin);
-//     }
+namespace Screen
+{
+    SSD1306::SSD1306(int width, int height, int resetPin)
+    {
+        this->screenWidth = width;
+        this->screenHeight = height;
+        this->display = Adafruit_SSD1306(width, height, &Wire, resetPin);
+    }
 
-//     void SSD1306::render() {
-//         this->display.display();
-//     }
+    void SSD1306::render()
+    {
+        this->display.display();
+    }
 
-//     void SSD1306::clear() {
-//         this->display.clearDisplay();
-//     }
+    void SSD1306::clear()
+    {
+        this->display.clearDisplay();
+    }
 
-//     void SSD1306::init() {
-//         this->display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-//     }
+    void SSD1306::init()
+    {
+        if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+        { // Address 0x3D for 128x64
+            Serial.println(F("SSD1306 allocation failed"));
+            for (;;)
+                ;
+        }
+    }
 
-//     void SSD1306::fillScreen(int color) {
-//         this->display.fillScreen(color);
-//     }
+    void SSD1306::fillScreen(int color)
+    {
+        this->display.fillScreen(color);
+    }
 
-//     void SSD1306::drawBitmap(int x, int y, const unsigned char* bitmap, int color, int width, int height) {
-//         this->display.drawBitmap(x, y, bitmap, width, height, color);
-//     }
+    void SSD1306::drawBitmap(int x, int y, const unsigned char *bitmap, int color, int width, int height)
+    {
+        this->display.drawBitmap(x, y, bitmap, width, height, color);
+    }
 
-//     void SSD1306::drawText(int x, int y, int size, int color, const char* text) {
-//         this->display.setTextSize(size);
-//         this->display.setTextColor(color);
-//         this->display.setCursor(x, y);
-//         this->display.print(text);
-//     }
+    void SSD1306::drawText(int x, int y, int size, int color, const char *text)
+    {
+        this->display.setTextSize(size);
+        this->display.setTextColor(color);
+        this->display.setCursor(x, y);
+        this->display.print(text);
+    }
 
-//     void SSD1306::setScreenWidth(int width) {
-//         this->screenWidth = width;
-//     }
+    void SSD1306::setScreenWidth(int width)
+    {
+        this->screenWidth = width;
+    }
 
-//     void SSD1306::setScreenHeight(int height) {
-//         this->screenHeight = height;
-//     }
+    void SSD1306::setScreenHeight(int height)
+    {
+        this->screenHeight = height;
+    }
 
-//     int SSD1306::getScreenWidth() {
-//         return this->screenWidth;
-//     }
+    int SSD1306::getScreenWidth()
+    {
+        return this->screenWidth;
+    }
 
-//     int SSD1306::getScreenHeight() {
-//         return this->screenHeight;
-//     }
-// }
+    int SSD1306::getScreenHeight()
+    {
+        return this->screenHeight;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <Arduino.h>
-#include "Components/Sequencer/Sequencer.hpp"
-#include "Inputs/Potentiometer/Potentiometer.hpp"
+// #include "Components/Sequencer/Sequencer.hpp"
+// #include "Inputs/Potentiometer/Potentiometer.hpp"
+#include "Screen/SSD1306/SSD1306.hpp"
+#include "bitmap/bli7.hpp"
 
-Components::Sequencer<uint16_t> sequencer(16, 180);
-InputsElt::Potentiometer potentiometer(0, 0, 1023, 60, 240);
+// Components::Sequencer<uint16_t> sequencer(16, 180);
+// InputsElt::Potentiometer potentiometer(0, 0, 1023, 60, 240);
+Screen::SSD1306 display(128, 64);
 
 void setup()
 {
 	Serial1.begin(115200);
-	sequencer.getDisplay().setup();
-	sequencer.getInputs().setup();
+	// sequencer.getDisplay().setup();
+	// sequencer.getInputs().setup();
+	display.init();
 }
 
 void loop()
 {
-	int pot_val = potentiometer.get();
-	sequencer.setBPM(pot_val);
-	sequencer.update();
-	sequencer.display();
+	display.fillScreen(BLACK);
+	display.drawBitmap(0, 0, bli7_text, WHITE, 128, 64);
+	display.render();
+	// int pot_val = potentiometer.get();
+	// sequencer.setBPM(pot_val);
+	// sequencer.update();
+	// sequencer.display();
 }
